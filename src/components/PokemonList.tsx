156b8d9c228af4205e2db23e395ad33e2030211a
Christian Günther <17281422+chrgue@users.noreply.github.com>
2024-01-12 16:52:10 +0100

    increase elements

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -9,7 +9,7 @@ export const PokemonList = ({text}: PokemonListProps) => {
         <>
             <div style={{display: "flex", flexWrap: "wrap"}}>
                 {
-                    Array.from({length: 2}).map((value) =>
+                    Array.from({length: 5}).map((value) =>
                         pokemons
                             .filter((p) => p.name.english.toLowerCase().includes(text.toLowerCase()))
                             .map((p) => <PokemonView key={`${value}-${p.id}`} pokemon={p}/>)
